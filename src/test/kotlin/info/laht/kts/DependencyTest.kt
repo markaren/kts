c3846f63873a3038141b7c05e9060c3a9162d80a
Lars Ivar Hatledal <laht@ntnu.no>
2020-08-12 14:24:12 +0200

    update

---
+++ src/test/kotlin/info/laht/kts/DependencyTest.kt
@@ -0,0 +1,15 @@
+package info.laht.kts
+
+import org.junit.jupiter.api.Test
+
+//https://stackoverflow.com/questions/40813062/maven-get-all-dependencies-programmatically
+//https://stackoverflow.com/questions/39638138/find-all-direct-dependencies-of-an-artifact-on-maven-central/39641359#39641359
+internal class DependencyTest {
+
+    @Test
+    fun test() {
+
+
+    }
+
+}
