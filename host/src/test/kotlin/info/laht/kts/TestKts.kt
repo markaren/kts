5b52a3cee7e4399e890bac86fbb8a6b9bae27cb5
Lars Ivar Hatledal <laht@hials.no>
2021-02-08 10:32:15 +0100

    update test

--- host/src/test/kotlin/info/laht/kts/TestKts.kt
+++ host/src/test/kotlin/info/laht/kts/TestKts.kt
@@ -44,27 +44,20 @@ internal class TestKts {
         @file:Repository("https://dl.bintray.com/ntnu-ihb/mvn")
 
         @file:DependsOn("no.ntnu.ihb.vico:core:0.3.3", options = arrayOf("scope=compile,runtime"))
-        @file:DependsOn("no.ntnu.ihb.vico:threekt-render:0.3.3", options = arrayOf("scope=compile,runtime"))
 
         @file:CompilerOptions("-jvm-target", "1.8")
 
-        import info.laht.krender.threekt.ThreektRenderer
         import no.ntnu.ihb.vico.components.Transform
         import no.ntnu.ihb.vico.core.EngineBuilder
         import no.ntnu.ihb.vico.render.Geometry
         import no.ntnu.ihb.vico.render.GeometryRenderer
         import no.ntnu.ihb.vico.render.mesh.BoxMesh
 
-        val renderer = ThreektRenderer()
-        EngineBuilder().renderer(renderer).build().use { engine ->
+        EngineBuilder().build().use { engine ->
 
             val e1 = engine.createEntity("e1")
             e1.add<Transform>()
             e1.add(Geometry(BoxMesh()))
-
-            engine.addSystem(GeometryRenderer())
-
-            engine.runner.startAndWait()
 
         }
         
